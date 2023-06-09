2938870ac3db574ce0b4c562e07ac92950d9a2ba
adrianrut <adrianrutkowskiarcgas@gmail.com>
2023-06-09 14:20:43 +0200

    Project create

---
+++ src/test/java/pl/rutkowski/gitapi/GitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rutkowski.gitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
